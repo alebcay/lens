3d1bd6eac9bce0b718d50d7a7d8a87a3af7db1aa
Sebastian Malton <sebastian@malton.name>
2022-11-03 16:13:24 -0400

    Add process of cleaning up lens proxy startup if down quickly
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/start-main-application/runnables/lens-proxy/request-app-version.injectable.ts
+++ src/main/start-main-application/runnables/lens-proxy/request-app-version.injectable.ts
@@ -3,17 +3,26 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
+import type { AbortSignal as NonStandardAbortSignal } from "abort-controller";
 import fetchInjectable from "../../../../common/fetch/fetch.injectable";
 import lensProxyPortInjectable from "../../../lens-proxy/lens-proxy-port.injectable";
 
+export interface RequestOptions {
+  signal: AbortSignal;
+}
+
+export type RequestAppVersionViaProxy = (options: RequestOptions) => Promise<string>;
+
 const requestAppVersionViaProxyInjectable = getInjectable({
   id: "request-app-version-via-proxy",
-  instantiate: (di) => {
+  instantiate: (di): RequestAppVersionViaProxy => {
     const lensProxyPort = di.inject(lensProxyPortInjectable);
     const fetch = di.inject(fetchInjectable);
 
-    return async () => {
-      const response = await fetch(`http://127.0.0.1:${lensProxyPort.get()}/version`);
+    return async (options) => {
+      const response = await fetch(`http://127.0.0.1:${lensProxyPort.get()}/version`, {
+        signal: options.signal as NonStandardAbortSignal,
+      });
 
       return (await response.json()).version as string;
     };
