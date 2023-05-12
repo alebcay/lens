28538c1c6d6724f658f4a0a170e3b9c8d207fc4e
Sebastian Malton <sebastian@malton.name>
2023-05-12 09:48:08 -0400

    chore: Rename sidebar item injectable files
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/renderer/components/config-validating-webhook-configurations/validating-webhook-configurations-sidebar-items.injectable.ts
+++ packages/core/src/renderer/components/config-validating-webhook-configurations/validating-webhook-configurations-sidebar-items.injectable.ts
@@ -7,7 +7,7 @@ import { sidebarItemInjectionToken } from "@meniscus/cluster-sidebar";
 import routeIsActiveInjectable from "../../routes/route-is-active.injectable";
 import validatingWebhookConfigurationsRouteInjectable from "../../../common/front-end-routing/routes/cluster/config/validating-webhook-configurations/validating-webhook-configurations-route.injectable";
 import navigateToValidatingWebhookConfigurationsInjectable from "../../../common/front-end-routing/routes/cluster/config/validating-webhook-configurations/navigate-to-validating-webhook-configurations.injectable";
-import configSidebarItemInjectable from "../config/config-sidebar-items.injectable";
+import configSidebarItemInjectable from "../config/config-sidebar-item.injectable";
 
 const validatingWebhookConfigurationsSidebarItemInjectable = getInjectable({
   id: "validating-webhook-configurations-sidebar-item",
